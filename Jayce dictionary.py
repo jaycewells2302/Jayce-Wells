37c663bd119e70fc0cc555d951dd9c27f3c7ebc4
Jayce Anthony Wells <jayce.wells@stu.jefferson.kyschools.us>
2018-03-19 09:35:07 -0400

    second commit

---
+++ Jayce dictionary.py
@@ -0,0 +1,5 @@
+basketball_positions = {
+    "guard": "controls the flow and tempo of the team, normally calls plays and brings ball up the court",
+    "forward": "ideally a good player on both sides of the court, scoring and defending, good size",
+    "center": "biggest guy on the court, offense normally runs through them because they are more difficult to defend",
+    }
