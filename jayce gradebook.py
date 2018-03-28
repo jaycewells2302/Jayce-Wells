12eb2fcbd5f0a0ba88378b27945537c0890a6213
Jayce Anthony Wells <jayce.wells@stu.jefferson.kyschools.us>
2018-03-28 09:30:28 -0400

    third commit

---
+++ jayce gradebook.py
@@ -0,0 +1,15 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Answer these questions...
+what is your grade in math?: 50
+what is your grade in history?: 50
+what is your grade in science?: 50
+what is your grade in english?: 50
+what is your grade in computer?: 50
+(50, 50, 50, 50, 50)
+your average is:
+50.0
+do better
+>>> 
